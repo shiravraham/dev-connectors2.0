fa21b09bdfbf599d72c17be945bb27b46cb2ac8e
Shir <shir.avraham13@gmail.com>
2022-02-08 13:36:33 +0200

    api get profile by user id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -4,8 +4,7 @@ const auth = require("../../middleware/auth");
 const Profile = require("../../models/Profile");
 const { check, validationResult } = require("express-validator");
 const normalize = require("normalize-url");
-
-router.get("/", (req, res) => res.send("Profile route"));
+const checkObjectId = require("../../middleware/checkObjectId");
 
 router.get("/me", auth, async (req, res) => {
   try {
@@ -23,6 +22,35 @@ router.get("/me", auth, async (req, res) => {
     res.status(500).send("Server Error");
   }
 });
+
+router.get("/", async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate("user", ["name", "avatar"]);
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+router.get(
+  "/user/:user_id",
+  checkObjectId("user_id"),
+  async ({ params: { user_id } }, res) => {
+    try {
+      const profile = await Profile.findOne({
+        user: user_id,
+      }).populate("user", ["name", "avatar"]);
+
+      if (!profile) return res.status(400).json({ msg: "Profile not found" });
+
+      return res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      return res.status(500).json({ msg: "Server error" });
+    }
+  }
+);
 
 router.post(
   "/",
