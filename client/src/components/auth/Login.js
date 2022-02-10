4268bd6ce3dc38486cd68aeba9571836edb6c82b
Shir <shir.avraham13@gmail.com>
2022-02-10 17:00:35 +0200

    login component state

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,5 +1,62 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
+import { Link } from "react-router-dom";
 
-const Login = () => <div>Login</div>;
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    const user = {
+      email,
+      password,
+    };
+
+    try {
+      console.log(user);
+    } catch (error) {
+      console.error(error.data);
+    }
+  };
+
+  return (
+    <Fragment>
+      <h1 className="large text-primary">Sign In</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Sign In Your Account
+      </p>
+      <form className="form" onSubmit={(e) => onSubmit(e)}>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <small className="form-text">
+            This site uses Gravatar so if you want a profile image, use a
+            Gravatar email
+          </small>
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            minLength="6"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Sign In" />
+      </form>
+      <p className="my-1">
+        Don't have an account? <Link to="/register">Register</Link>
+      </p>
+    </Fragment>
+  );
+};
 
 export default Login;
