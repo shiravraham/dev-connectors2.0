085aa07d6a42b244bad886cb7c23c91a70f0deed
Shir <shir.avraham13@gmail.com>
2022-02-10 15:39:07 +0200

    added react router and connected login & register

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
 import React, { Fragment } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 import "./App.css";
 
-const App = () => <Fragment>
-  <Navbar/>
-  <Landing/>
-</Fragment>;
+const App = () => (
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Landing />} />
+      </Routes>
+      <section className="container">
+        <Routes>
+          <Route path="register" element={<Register />} />
+          <Route path="login" element={<Login />} />
+        </Routes>
+      </section>
+    </Fragment>
+  </Router>
+);
 export default App;
