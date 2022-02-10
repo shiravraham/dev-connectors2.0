26bfe7e071883322846c3200c7670b816af00923
Shir <shir.avraham13@gmail.com>
2022-02-10 14:50:43 +0200

    added navbar & landing components

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,10 @@
 import React, { Fragment } from "react";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
 import "./App.css";
 
-const App = () => <Fragment className="App">Shir</Fragment>;
+const App = () => <Fragment>
+  <Navbar/>
+  <Landing/>
+</Fragment>;
 export default App;
