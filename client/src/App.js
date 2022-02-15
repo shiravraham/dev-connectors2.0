35013f74caa8d6854ec849a15fcf72853939a873
Shir <shir.avraham13@Gmail.com>
2022-02-15 11:12:00 +0200

    implemented user loaded & auth fail on register

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
@@ -8,8 +8,20 @@ import "./App.css";
 import Alert from "./components/layout/alert";
 import {Provider} from 'react-redux';
 import store from "./store";
+import { loadUser } from "./actions/auth";
+import setAuthToken from "./utils/setAuthToken";
 
-const App = () => (
+if(localStorage.token){
+  setAuthToken(localStorage.token);
+}
+
+const App = () => {
+  
+useEffect( ()=>{
+  store.dispatch(loadUser());
+}, [])
+
+  return (
   <Provider store={store}>
   <Router>
     <Fragment>
@@ -27,5 +39,5 @@ const App = () => (
     </Fragment>
   </Router>
   </Provider>
-);
+)};
 export default App;
